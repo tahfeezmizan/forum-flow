810981d81894eab69910cc6832f3d3658fe5c06e
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-03-08 18:54:37 +0600

    responsive banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      colors: {
+        clifford: '#da373d',
+        primaryColor: 'rgb(18, 19, 45);'
+      }
     },
   },
   plugins: [],
