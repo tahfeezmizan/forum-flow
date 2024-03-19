76514b2f199fda863f298b9bceaf53a7170d9dc1
tahfeezmizan <mizanahmed5316@gmial.com>
2024-03-19 22:28:42 +0600

    load post, display

--- src/script/script.js
+++ src/script/script.js
@@ -47,3 +47,91 @@ const fetchData = (search) => {
         })
 
 }
+
+const searchDataByCategories = () => {
+    toggleLoadingSpinner(true);
+
+    const searchField = document.getElementById('search-filed');
+    const searchText = searchField.value;
+    searchField.value = ''
+    postContainer.innerHTML = '';
+
+    fetchData(searchText)
+}
+
+// loading spinner function 
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden')
+
+        setTimeout(() => {
+            toggleLoadingSpinner(false);
+        }, 2000);
+    }
+    else {
+        loadingSpinner.classList.add('hidden')
+    }
+}
+
+
+// finished forum item add
+const ReadTotalForum = document.getElementById('totalReadForum');
+let readItemIncrement = parseInt(ReadTotalForum.innerText || '0');
+
+function clickBtn(title, view) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('flex', 'gap-4', 'items-center', 'bg-white', 'rounded-2xl', 'p-4', 'hover:bg-blue-50', 'duration-300');
+    newDiv.innerHTML = `
+        <h3 class="mulish text-lg font-bold">${title}</h3>
+        <p class="inter text-base text-black opacity-60 flex items-center gap-3"><i
+                class="fa-regular fa-eye"></i> ${view}</p>
+    `;
+    finishedRead.appendChild(newDiv);
+    readItemIncrement++;
+    ReadTotalForum.innerText = readItemIncrement;
+}
+
+// latest post data load 
+const latestPostLoad = () => {
+    const url = 'https://openapi.programming-hero.com/api/retro-forum/latest-posts';
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            const postItem = { data }.data
+            postItem.forEach(data => {
+                const newPostDiv = document.createElement('div');
+                newPostDiv.innerHTML = `
+                <div class="card w-{340px} md:w-96 mx-auto bg-base-100 shadow-xl">
+                    <figure class="px-6 lg:px-10 pt-10">
+                        <img src="${data.cover_image}" alt="Shoes"
+                            class="rounded-xl" />
+                    </figure>
+                    <div class="card-body px-6 lg:px-10">
+                        <p class="text-base mulish text-black opacity-70 flex gap-3 items-center"><i
+                                class="fa-regular fa-calendar-days"></i>${data.author?.posted_date ? data.author?.posted_date : 'no date published'}</p>
+                        <h2 class="card-title mulish text-lg font-extrabold text-black opacity-80 mb-2">${data.title}</h2>
+                        <p class="mulish text-base text-black opacity-70 font-normal mb-4">${data.description}</p>
+
+                        <div id="" class="flex gap-5 items-center">
+                            <div>
+                                <img class="w-12 h-12 rounded-full" src="${data.profile_image}"
+                                    alt="">
+                            </div>
+                            <div class="">
+                                <h2 class="font-bold text-base pb-1">${data.author?.name}</h2>
+                                <h4 class="text-sm pb-2">${data.author?.designation ? data.author?.designation : 'Unknown'}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div> `
+                latestPost.appendChild(newPostDiv);
+            })
+        })
+}
+
+latestPostLoad()
+fetchData('')
+
+
